3bbbd0d21c8864f17de3eadee49541be991daad6
NicoLiu <liuxinq@1000mob.com>
2018-04-10 16:58:10 +0800

    增加修改步数定时任务

--- src/main/java/com/love/NameOfLoveWebApplication.java
+++ src/main/java/com/love/NameOfLoveWebApplication.java
@@ -2,11 +2,13 @@ package com.love;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NameOfLoveWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NameOfLoveWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NameOfLoveWebApplication.class, args);
+    }
 }
---
+++ src/main/java/com/love/task/UpdateUserStep.java
@@ -0,0 +1,22 @@
+package com.love.task;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.love.mapper.UserDAO;
+
+@Component
+public class UpdateUserStep {
+    private static final Logger logger = LoggerFactory.getLogger(UpdateUserStep.class);
+    @Resource
+    private UserDAO userDAO;
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void doTask() {
+        userDAO.updateStep(null, 0);
+    }
+}
